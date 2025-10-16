de7ca84837ad7f9f2b1f0970615d2781e11b53bd
Raphael Bessin <bessin.raphael@gmail.com>
2025-10-16 14:26:35 -0400

    added comments to RichTextEditor

--- components/editor/RichTextEditor.tsx
+++ components/editor/RichTextEditor.tsx
@@ -2,8 +2,11 @@
 
 import dynamic from 'next/dynamic';
 
+// Creates a dynamic import of Quill Editor
 const QuillEditor = dynamic(() => import('./QuillEditor'), {
+  // Blocks server side rendering
   ssr: false,
+  // Server renders loading while client renders the editor
   loading: () => (
     <div className="flex-1 flex items-center justify-center">
       <p>Loading editor...</p>
@@ -11,12 +14,14 @@ const QuillEditor = dynamic(() => import('./QuillEditor'), {
   )
 });
 
+// Defined the prop interface for the Quill Editor
 interface RichTextEditorProps {
   fileContent?: string;
   fileName?: string;
   onContentChange?: (getContent: () => Promise<{ content: string; fileName: string }>) => void;
 }
 
+// Exports the dynamically loaded Quill Editor component with its props
 export default function RichTextEditor({ fileContent, fileName, onContentChange }: RichTextEditorProps) {
   return <QuillEditor fileContent={fileContent} fileName={fileName} onContentChange={onContentChange} />;
 }
